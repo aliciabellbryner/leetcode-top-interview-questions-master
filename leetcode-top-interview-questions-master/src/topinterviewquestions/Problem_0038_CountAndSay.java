cf7ffe2cd947ae3c8be56bcacf1524a96238c40a
aliciabellbryner <92649245+aliciabellbryner@users.noreply.github.com>
2021-10-16 20:05:18 -0500

    Update Problem_0038_CountAndSay.java

--- leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0038_CountAndSay.java
+++ leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0038_CountAndSay.java
@@ -9,25 +9,25 @@ public class Problem_0038_CountAndSay {
 		if (n == 1) {
 			return "1";
 		}
-		char[] last = countAndSay(n - 1).toCharArray();
+		String last = countAndSay(n - 1);
 		StringBuilder ans = new StringBuilder();
 		int times = 1;
-		for (int i = 1; i < last.length; i++) {
-			if (last[i - 1] == last[i]) {
+		for (int i = 1; i < last.length(); i++) {
+			if (last.charAt(i-1) == last.charAt(i)) {
 				times++;
 			} else {
-				ans.append(String.valueOf(times));
-				ans.append(String.valueOf(last[i - 1]));
+				ans.append(times);
+				ans.append(last.charAt(i-1));
 				times = 1;
 			}
 		}
-		ans.append(String.valueOf(times));
-		ans.append(String.valueOf(last[last.length - 1]));
+		ans.append(times);
+		ans.append(last.charAt(last.length() - 1));
 		return ans.toString();
 	}
 
 	public static void main(String[] args) {
-		System.out.println(countAndSay(20));
+		System.out.println(countAndSay(5));
 	}
 
 }
