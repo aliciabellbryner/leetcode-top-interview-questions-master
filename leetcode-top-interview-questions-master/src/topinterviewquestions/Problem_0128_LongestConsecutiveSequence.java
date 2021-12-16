6dd5ee6af6673aa12f2c5bbe909992d06117df23
aliciabellbryner <92649245+aliciabellbryner@users.noreply.github.com>
2021-12-16 15:51:00 -0600

    1216

--- leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0128_LongestConsecutiveSequence.java
+++ leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0128_LongestConsecutiveSequence.java
@@ -1,8 +1,32 @@
 package topinterviewquestions;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Problem_0128_LongestConsecutiveSequence {
+
+
+	//time O(N), space O(N)
+	public static int longestConsecutive_leetcode(int[] nums) {
+		HashSet<Integer> set = new HashSet<>();
+		for (int i : nums) {
+			set.add(i);
+		}
+		int res = 0;
+		int curLink;
+		for (int i : set) {
+			if (!set.contains(i - 1)) {
+				curLink = 1;
+				while (set.contains(i + 1)) {
+					curLink++;
+					i++;
+				}
+				res = Math.max(res, curLink);
+			}
+		}
+		return res;
+	}
+
 
 	public static int longestConsecutive(int[] nums) {
 		HashMap<Integer, Integer> map = new HashMap<>();
@@ -19,6 +43,19 @@ public class Problem_0128_LongestConsecutiveSequence {
 			}
 		}
 		return len;
+	}
+
+	public static void main(String[] args) {
+		HashSet<Integer> set = new HashSet<>();
+		set.add(9);
+		set.add(2);
+		set.add(8);
+//		for (int i : set) {
+//			System.out.println(i);
+//		}
+
+		int[] test = new int[]{100,4,200,1,3,2};
+		System.out.println(longestConsecutive_leetcode(test));
 	}
 
 }
