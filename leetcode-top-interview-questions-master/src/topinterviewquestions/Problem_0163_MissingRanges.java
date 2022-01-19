5c0b8a5f88b6cd3649160c9e728fd6914b51f8b4
aliciabellbryner <92649245+aliciabellbryner@users.noreply.github.com>
2022-01-19 14:16:30 -0600

    0119-1

--- leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0163_MissingRanges.java
+++ leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0163_MissingRanges.java
@@ -26,7 +26,7 @@ public class Problem_0163_MissingRanges {
 		}
 		return res;
 	}
-	public String miss_j(int lower, int upper) {
+	public String miss(int lower, int upper) {
 		StringBuilder sb = new StringBuilder();
 		sb.append(lower);
 		if (upper > lower) {
@@ -35,32 +35,32 @@ public class Problem_0163_MissingRanges {
 		return sb.toString();
 	}
 
-
-	public static List<String> findMissingRanges(int[] nums, int lower, int upper) {
-		List<String> ans = new ArrayList<>();
-		for (int num : nums) {
-			if (num > lower) {
-				ans.add(miss(lower, num - 1));
-			}
-			if (num == upper) {
-				return ans;
-			}
-			lower = num + 1;
-		}
-		if (lower <= upper) {
-			ans.add(miss(lower, upper));
-		}
-		return ans;
-	}
-
-	// 生成"lower->upper"的字符串，如果lower==upper，只用生成"lower"
-	public static String miss(int lower, int upper) {
-		String left = String.valueOf(lower);
-		String right = "";
-		if (upper > lower) {
-			right = "->" + String.valueOf(upper);
-		}
-		return left + right;
-	}
+// zuo's solution, not good miss func
+//	public static List<String> findMissingRanges(int[] nums, int lower, int upper) {
+//		List<String> ans = new ArrayList<>();
+//		for (int num : nums) {
+//			if (num > lower) {
+//				ans.add(miss(lower, num - 1));
+//			}
+//			if (num == upper) {
+//				return ans;
+//			}
+//			lower = num + 1;
+//		}
+//		if (lower <= upper) {
+//			ans.add(miss(lower, upper));
+//		}
+//		return ans;
+//	}
+//
+//	// 生成"lower->upper"的字符串，如果lower==upper，只用生成"lower"
+//	public static String miss(int lower, int upper) {
+//		String left = String.valueOf(lower);
+//		String right = "";
+//		if (upper > lower) {
+//			right = "->" + String.valueOf(upper);
+//		}
+//		return left + right;
+//	}
 
 }
