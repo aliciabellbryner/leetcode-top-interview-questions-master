d16c2d518d47d5aa204665657966bd9a4f28907a
aliciabellbryner <92649245+aliciabellbryner@users.noreply.github.com>
2022-01-23 00:16:29 -0600

    class update

--- leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_1910_RemoveAllOccurrencesofaSubstring_G.java
+++ leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_1910_RemoveAllOccurrencesofaSubstring_G.java
@@ -1,4 +1,7 @@
 package topinterviewquestions;
 
 public class Problem_1910_RemoveAllOccurrencesofaSubstring_G {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
