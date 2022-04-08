c80f2b9002ce909bc0bcd91a8a786de57673c4c3
aliciabellbryner <92649245+aliciabellbryner@users.noreply.github.com>
2022-04-08 01:03:10 -0500

    0408

--- leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0205_IsomorphicStrings_G.java
+++ leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0205_IsomorphicStrings_G.java
@@ -1,5 +1,31 @@
 package topinterviewquestions;
 
-public class Problem_0205_IsomorphicStrings_G {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Problem_0205_IsomorphicStrings_G {
+    //solution: 思路就是把string转变成每个char在string中最早出现的index
+    //比如s = "paper", t = "title"，s就变成0 1 0 3 4, t就变成0 1 0 3 4
+    class Solution {
+        private String transformString(String s) {
+            Map<Character, Integer> indexMapping = new HashMap<>();
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < s.length(); ++i) {
+                char c1 = s.charAt(i);
+
+                if (!indexMapping.containsKey(c1)) {
+                    indexMapping.put(c1, i);
+                }
+
+                builder.append(Integer.toString(indexMapping.get(c1)));
+                builder.append(" ");
+            }
+            return builder.toString();
+        }
+
+        public boolean isIsomorphic(String s, String t) {
+            return transformString(s).equals(transformString(t));
+        }
+    }
 }
