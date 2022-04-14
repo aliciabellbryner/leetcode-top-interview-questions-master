c8c6de7edaac8380f5d630544cb8206510cee9a5
aliciabellbryner <92649245+aliciabellbryner@users.noreply.github.com>
2022-04-14 14:37:21 -0500

    0414

--- leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0310_MinimumHeightTrees_G.java
+++ leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0310_MinimumHeightTrees_G.java
@@ -1,4 +1,87 @@
 package topinterviewquestions;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Problem_0310_MinimumHeightTrees_G {
+    //https://leetcode.com/problems/minimum-height-trees/solution/
+    /*
+    Let |V| be the number of nodes in the graph, then the number of edges would be |V| - 1∣V∣−1 as specified in the problem.
+
+Time Complexity: O(∣V∣)
+
+First, it takes ∣V∣−1 iterations for us to construct a graph, given the edges.
+
+With the constructed graph, we retrieve the initial leaf nodes, which takes ∣V∣ steps.
+
+During the BFS trimming process, we will trim out almost all the nodes (|V|) and edges (|V|-1∣V∣−1) from the edges. Therefore, it would take us around ∣V∣+∣V∣−1 operations to reach the centroids.
+
+To sum up, the overall time complexity of the algorithm is O(∣V∣).
+
+Space Complexity: O(∣V∣)
+
+We construct the graph with adjacency list, which has ∣V∣ nodes and ∣V∣−1 edges. Therefore, we would need ∣V∣+∣V∣−1 space for the representation of the graph.
+
+In addition, we use a queue to keep track of the leaf nodes. In the worst case, the nodes form a star shape, with one centroid and the rest of the nodes as leaf nodes. In this case, we would need |V|-1∣V∣−1 space for the queue.
+
+To sum up, the overall space complexity of the algorithm is also O(∣V∣).
+     */
+    //思路就是把它看成是找这个图的中心点，把最外一层也就是neighbor只有一个的node放进leaf的集合里，然后再逐层剥离，直到最后剩一个或两个的时候就是我们要的结果了
+    class Solution {
+        public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+
+            // edge cases
+            if (n < 2) {
+                ArrayList<Integer> res = new ArrayList<>();
+                for (int i = 0; i < n; i++)
+                    res.add(i);
+                return res;
+            }
+
+            // Build the graph with the adjacency list
+            ArrayList<Set<Integer>> neighbors = new ArrayList<>();//index表示哪个位置的node，值list代表它的neighbors
+            for (int i = 0; i < n; i++) {
+                neighbors.add(new HashSet<Integer>());
+            }
+
+            for (int[] edge : edges) {
+                Integer start = edge[0], end = edge[1];
+                neighbors.get(start).add(end);
+                neighbors.get(end).add(start);
+            }
+
+            // Initialize the first layer of res
+            ArrayList<Integer> res = new ArrayList<>();
+            for (int i = 0; i < n; i++)
+                if (neighbors.get(i).size() == 1)
+                    res.add(i);
+
+            // Trim the res until reaching the centroids
+            int remainingNodes = n;
+            while (remainingNodes > 2) {//The iteration terminates when there are no more than two nodes left in the graph, which are the desired centroids nodes.
+                remainingNodes -= res.size();
+                ArrayList<Integer> newLeaves = new ArrayList<>();
+
+                // remove the current res along with the edges
+                for (Integer leaf : res) {
+                    // the only neighbor left for the leaf node
+                    for (Integer neighbor : neighbors.get(leaf)) {
+//                    Integer neighbor = neighbors.get(leaf).iterator().next();
+                        // remove the edge along with the leaf node
+                        neighbors.get(neighbor).remove(leaf);
+                        if (neighbors.get(neighbor).size() == 1)
+                            newLeaves.add(neighbor);
+                    }
+                }
+
+                // prepare for the next round
+                res = newLeaves;
+            }
+
+            // The remaining nodes are the centroids of the graph
+            return res;
+        }
+    }
 }
--- leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0316_RemoveDuplicateLetters_G.java
+++ leetcode-top-interview-questions-master/src/topinterviewquestions/Problem_0316_RemoveDuplicateLetters_G.java
@@ -1,4 +1,36 @@
 package topinterviewquestions;
 
 public class Problem_0316_RemoveDuplicateLetters_G {
+    public class Solution {
+        //https://leetcode.com/problems/remove-duplicate-letters/discuss/76769/Java-solution-using-Stack-with-comments/80556
+
+        public String removeDuplicateLetters(String s) {
+
+            int[] freq = new int[26]; // will contain number of occurences of character (i+'a')
+            boolean[] visited = new boolean[26]; // will contain if character ('a' + i) is present in current result Stack
+            for(char c : s.toCharArray()){  // count number of occurences of character
+                freq[c-'a']++;
+            }
+            StringBuilder sb = new StringBuilder();; // answer stack
+            int index;
+            for(char c : s.toCharArray()){
+                index = c - 'a';
+                freq[index]--;   // decrement number of characters remaining in the string to be analysed
+                if(visited[index]) { // if character is already present in stack, don't bother
+                    continue;
+                }
+                // if current character is smaller than last character in stack which occurs later in the string again
+                // it can be removed and  added later e.g stack = bc remaining string abc then a can pop b and then c
+                while( (sb.length() > 0) && c < sb.charAt(sb.length()-1) && freq[sb.charAt(sb.length()-1)-'a'] != 0){
+                    visited[sb.charAt(sb.length()-1) - 'a'] = false;
+                    sb.deleteCharAt(sb.length()-1);
+                }
+                sb.append(c); // add current character and mark it as visited
+                visited[index] = true;
+            }
+
+            return sb.toString();
+
+        }
+    }
 }
